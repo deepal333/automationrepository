67d5b2338002d05724b84d442f0d2e5a1d0c0df1
pradeep bombe <bombepradeep@gmail.com>
2019-09-24 21:50:39 +0530

    Variables & Datatypes

---
+++ BasicCoreJava/src/variablesandatatypes/FirstJavaClass.java
@@ -0,0 +1,26 @@
+package variablesandatatypes;
+
+public class FirstJavaClass {
+	
+	public static void main(String[] args)
+	{
+		int a=10;//For Values
+		char b='A';//For Variables
+		boolean c=true;//For Boolean Values
+		double d=6.89;//For Decimal Values
+		String name="Pradeep";//For String Values *String is not data type
+		
+		//Other Data Types:
+		//For Numbers
+		byte x=10;
+		short y=45;
+		long z=678954;
+		
+		//For Decimal Values
+		float f=67.8f;
+		
+		System.out.println(a);
+		
+	}
+
+}
